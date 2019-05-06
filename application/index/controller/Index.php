a5355151623ef91deaeb1d36185ea4b78ede2989
ahtcwangjian <ahtcwangjian@qq.com>
2019-05-06 17:03:05 +0800

    index

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -4,9 +4,9 @@
  * @Author: wangjian
  * @Date:   2019-05-06 16:35:08
  * @Last Modified by:   wangjian
- * @Last Modified time: 2019-05-06 16:49:57
+ * @Last Modified time: 2019-05-06 17:02:30
  */
-namespace app\pushmsg\controller;
+namespace app\index\controller;
 
 use think\Controller;
 
